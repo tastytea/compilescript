b4011aae38b03d84af01513bc14b04160fa2a6ac
tastytea <tastytea@tastytea.de>
2019-03-07 23:21:58 +0100

    Changed copyright notice.

--- src/main.cpp
+++ src/main.cpp
@@ -148,8 +148,8 @@ int main(int argc, char *argv[])
         cout << "compilescript " << global::version << endl <<
             "Copyright (C) 2018, 2019 tastytea <tastytea@tastytea.de>\n"
             "License GPLv3: GNU GPL version 3 <https://www.gnu.org/licenses/gpl-3.0.html>.\n"
-            "This is free software: you are free to change and redistribute it.\n"
-            "There is NO WARRANTY, to the extent permitted by law.\n";
+            "This program comes with ABSOLUTELY NO WARRANTY. This is free software,\n"
+            "and you are welcome to redistribute it under certain conditions.\n";
         return 0;
     }
 
